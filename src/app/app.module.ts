3e1541f64668c583ec3c12c4024f6d37807dc70b
andregsistemas <andregsistemas@outlook.com>
2024-09-12 10:56:04 -0300

    02. Entendendo o fluxo de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    RodapeComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
