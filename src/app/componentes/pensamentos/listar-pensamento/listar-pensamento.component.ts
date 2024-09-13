822b1db76ff73e9166ddd56856519d3fd21a7de5
andregsistemas <andregsistemas@outlook.com>
2024-09-13 11:12:03 -0300

    05.Comunicação com o Backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+   {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componmente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @input()',
+      autoria: 'Componente Filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pretium viverra euismod. Fusce luctus magna magna, eget dapibus enim posuere nec. Nulla placerat lacus ac velit tempor vulputate. Sed dapibus condimentum risus, ut faucibus nunc cursus at. Donec volutpat ante in ullamcorper aliquet. Sed non nunc et diam aliquet hendrerit vitae non justo. Pellentesque fermentum tincidunt pretium.',
+      autoria: 'Componente Filho',
+      modelo: 'modelo1'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
